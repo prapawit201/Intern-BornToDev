3bdc356b14a9af43ad6e7188afaf5aa54c1740c4
zBabEs <piyapat43@gmail.com>
2020-11-02 01:10:48 +0700

    [Start] Home Page

---
+++ frontend/src/page/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="container" id="test">
+          Home Hello
+        </div>
+      </div>
+    );
+  }
+}
