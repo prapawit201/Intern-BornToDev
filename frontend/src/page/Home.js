eaf2d73aa953b7bbb1b21b1e3750f69c02438598
zBabEs <32611645+prapawit201@users.noreply.github.com>
2020-11-02 15:19:47 +0700

    [Update] Home Page

--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -1,11 +1,18 @@
-import React from "react";
-
-export default class Home extends React.Component {
+// import React from "react";
+import React, { Fragment } from "react";
+import Navbar from "../component/Navbar"
+import pic from "../image/Scrum.png"
+import "../App.css";
+export default class OutlinedButtons extends React.Component {
   render() {
     return (
       <div>
-        <div className="container" id="test">
-          Home Hello
+        <Navbar/>
+        <center><img id="picHome"src={pic}/></center>
+        <div className="container">
+          <section id="enter">
+          <button type="button" class="btn btn-outline-info">Enter to Website</button>
+          </section>
         </div>
       </div>
     );
